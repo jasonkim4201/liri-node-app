7c77a98b216611442c08cf3447bc96d2e192939a
Jason Kim <jasonkim4201@gmail.com>
2019-01-24 20:12:53 -0500

    set up the concert-this function to work

--- liri.js
+++ liri.js
@@ -2,28 +2,77 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 const axios = require("axios");
+const Spotify = require('node-spotify-api');
+var moment = require('moment');
 
-//var spotify = new Spotify(keys.spotify);
+
+var spotify = new Spotify(keys.spotify);
 
 
 const command = process.argv[2];
-const name = process.argv.splice(3, process.argv.length).join("+");
+const name = process.argv.splice(3, process.argv.length).join(" ");
 
-//make functions for each command
 const concertThis = () => {
-  //use bands in town artists events API to search for artist and info about name of venue, venue location, and date of event (MM/DD/YYYY)
-    
+  
+  //GET NAME about name of venue, venue location, and date of event (MM/DD/YYYY)
+  //use axios. get function. use for loop to get data
+  
+
+  const searchUrl = `https://rest.bandsintown.com/artists/${name}/events?app_id=codingbootcamp`;
+  
+  axios.get(searchUrl)
+  .then(function (response) {
+
+    var search = response.data;
+
+    for (var i = 0; i < search.length; i++) {
+      var concertInfo = search[i];
+      //moment(stringofcodestuff).format(thestupidwayyouwanttimetoappear);
+      //you're telling me i can't spam emojis on node???
+      console.log(name);        
+      console.log(`
+      ${concertInfo.venue.name}
+      ${concertInfo.venue.city}, ${concertInfo.venue.region}
+      ${moment(concertInfo.datetime).format("L")}
+      `);
+      console.log("==========================================================================");
+      
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 
 }
 
-const spotifySong = (artist) => {
-  //get queryurl for spotify
+const spotifySong = () => {
+  
+  spotify
+  .search({ type: 'track', query: name ? name : "Boulevard of Broken Dreams"})
+  .then(function(response) {
+    //It's not stupid if it works okay????
+
+    const music = response.tracks.items[0];
+    console.log("Here is some info about the song you entered.")
+    console.log(`Artist: ${music.album.artists[0].name}`);
+    console.log(`Song name: ${music.name}`);
+    console.log(`Spotify link: ${music.album.artists[0].external_urls.spotify}`);
+    console.log(`Album name: ${music.album.name}`);
+
+
+  
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
 }
 
 const movieThis = () => {
   //get queryurl so you can actually search in the first place!
   //if user doesnt type in anything it will default to mr.nobody
+  //it works. not gonna comaplain
 
   const defaultName = "mr.nobody";
   const queryUrl = `http://www.omdbapi.com/?t=${name ? name : defaultName}&y=&plot=short&apikey=trilogy`;
@@ -31,7 +80,7 @@ const movieThis = () => {
   axios
   .get(queryUrl)
   .then((response) => {
-    //because I'm too lazy to repeat response.data
+    //look into that other method to shorten response.data and try using that
     const movie = response.data;
 
     console.log(`
@@ -59,7 +108,7 @@ switch (command) {
   return concertThis();
 
   case "spotify-this-song":
-  return spotifySong();
+  return spotifySong(); 
 
   case "movie-this":
   return movieThis();
